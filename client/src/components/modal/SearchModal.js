da9db7760e33fa953b6e688d34a7b669330ff19a
Andy Zhao <azhao24@hotmail.com>
2023-02-06 15:26:58 -0500

    updated search modal

--- client/src/components/modal/SearchModal.js
+++ client/src/components/modal/SearchModal.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import RestaurantData from "../../restaurants.json";
 import Modal from 'react-bootstrap/Modal';
 import '../../styles.css';
@@ -9,25 +9,48 @@ export const SearchModal = ({searchActive, handleSearch}) => {
     const handleChange = (e) => {
         e.preventDefault();
         setSearchInput(e.target.value);
-        console.log(searchInput);
     }
+
+    // useEffect(() => {
+    //     console.log("Search inside useEffect: ", searchInput);
+    // }, [searchInput]);
+
 
     return (
       <>
         <Modal show={searchActive} onHide={handleSearch} className="main-modal">
-          <Modal.Header closeButton>
-            <Modal.Title>Search by food</Modal.Title>
-            <div>Quickly display the food you're looking for alongside other restaurants
-                and compare that exact dish</div> 
+          <Modal.Header closeButton className="search-header">
+            <Modal.Title>
+                <div className="search-title">
+                    Search by food or restaurant
+                </div>
+                <div className="search-subtitle">
+                    Displays foods you are looking for alongside other restaurants and compare that exact dish
+                </div>
+            </Modal.Title>
           </Modal.Header>
           <Modal.Body className="modal-body">
-            <div>
-                <input 
-                    type="text"
-                    placeholder="Search Here"
+            <div className="main">
+                <div className="form-group has-search">
+                    <span className="fa fa-search form-control-feedback"></span>
+                    <input 
+                    type="text" 
+                    className="form-control"
+                    placeholder="Search"
                     onChange={handleChange}
                     value={searchInput}
-                />
+                    />
+                </div>
+            </div>
+            <div>
+                {/* {RestaurantData[0].restaurants.filter(restaurant =>
+                    restaurant.name === searchInput).map(filteredRestaurant => (
+                        console.log(filteredRestaurant.name)
+                    ))} */}
+                {RestaurantData[0].restaurants.filter(restaurant =>
+                    restaurant.name === searchInput).map(filteredRestaurant => (
+                        console.log(filteredRestaurant.name)
+                    ))}
             </div>
           </Modal.Body>
         </Modal>
